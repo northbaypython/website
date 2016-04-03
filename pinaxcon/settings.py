eb629fdad4a043c2108ef18ddd3f8a390f099a97
Christopher Neugebauer <chrisjrn@gmail.com>
2016-04-03 18:06:09 +1000

    more gauze

--- pinaxcon/settings.py
+++ pinaxcon/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     "default": dj_database_url.config()
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
