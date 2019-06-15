3ee7b88ad337a18cf205e5aebf263b45d265158d
Christopher Neugebauer <_@chrisjrn.com>
2019-06-15 16:10:23 -0500

    Disable some things

--- pinaxcon/urls.py
+++ pinaxcon/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     url(r"^speak$", TemplateView.as_view(template_name="static_pages/speak.html"), name="speak"),
 
     # attend
-    url(r"^attend$", TemplateView.as_view(template_name="static_pages/attend/attend.html"), name="attend/attend"),
+    # url(r"^attend$", TemplateView.as_view(template_name="static_pages/attend/attend.html"), name="attend/attend"),
     url(r"^tickets$", RedirectView.as_view(url="attend")),
     url(r"^tickets/buy$", views.buy_ticket, name="buy_ticket"),
     url(r"^attend/business-case$", TemplateView.as_view(template_name="static_pages/attend/business-case.html"), name="attend/business-case"),
@@ -45,10 +45,10 @@ urlpatterns = [
     url(r"^attend/finaid$", RedirectView.as_view(url="/opportunity-grant")),
     url(r"^attend/financial-aid$", RedirectView.as_view(url="/opportunity-grant")),
 
-    url(r"^attend/stay$", TemplateView.as_view(template_name="static_pages/attend/travel.html"), name="attend/travel"),
-    url(r"^attend/travel$", TemplateView.as_view(template_name="static_pages/attend/travel.html"), name="attend/travel"),
-    url(r"^attend/hotels$", TemplateView.as_view(template_name="static_pages/attend/hotels.html"), name="attend/hotels"),
-    url(r"^attend/tshirt$", TemplateView.as_view(template_name="static_pages/attend/tshirt.html"), name="attend/tshirt"),
+    # url(r"^attend/stay$", TemplateView.as_view(template_name="static_pages/attend/travel.html"), name="attend/travel"),
+    # url(r"^attend/travel$", TemplateView.as_view(template_name="static_pages/attend/travel.html"), name="attend/travel"),
+    # url(r"^attend/hotels$", TemplateView.as_view(template_name="static_pages/attend/hotels.html"), name="attend/hotels"),
+    # url(r"^attend/tshirt$", TemplateView.as_view(template_name="static_pages/attend/tshirt.html"), name="attend/tshirt"),
     url(r"^attend/accessibility-and-accommodations$",TemplateView.as_view(template_name="static_pages/attend/accommodations.html"), name="attend/accessibility-and-accommodations"),
     url(r"^accessibility$", RedirectView.as_view(url="attend/accessibility-and-accommodations")),
     url(r"^accommodations$", RedirectView.as_view(url="attend/accessibility-and-accommodations")),
