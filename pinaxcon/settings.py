665f0b068c85d6f54eddf83a51a96599945d2163
Christopher Neugebauer <_@chrisjrn.com>
2019-06-29 15:11:12 +0800

    Use append_slash

--- pinaxcon/settings.py
+++ pinaxcon/settings.py
@@ -17,6 +17,8 @@ DATABASES = {
 
 UNPREPEND_WWW = bool(int(os.environ.get("DJANGO_UNPREPEND_WWW", "0")))
 
+APPEND_SLASH = True
+
 # HEROKU: Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
@@ -30,9 +32,9 @@ from_email = os.environ.get("DJANGO_DEFAULT_FROM_EMAIL", None)
 if from_email is not None:
     DEFAULT_FROM_EMAIL = from_email
     SERVER_EMAIL = DEFAULT_FROM_EMAIL
-    
+
 THEME_CONTACT_EMAIL = os.environ.get("THEME_CONTACT_EMAIL", None)
-    
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
